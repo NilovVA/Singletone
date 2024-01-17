e3367c7dd682273cdd72bdf818fb7531226f9755
Nilovva <slavka11118@yandex.ru>
2024-01-17 19:17:19 +0400

    Добавьте файлы проекта.

---
+++ singletone/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace singletone
+{
+ 
+    public class User
+    {
+        static User user;
+        protected User()
+        {
+            Console.WriteLine("Good day, user");
+        }
+        public static User Instance()
+        {
+            if (user == null)
+                user = new User();
+            else
+                Console.WriteLine("What do you want to find?");
+            return user;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            User a = User.Instance();
+            User b = User.Instance();
+        }
+    }
+}
+
